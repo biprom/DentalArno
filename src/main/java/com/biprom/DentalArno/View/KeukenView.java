11d293d9f8f96a905c72e07796db9ea4e4fa0446
Kristof Taveirne <kristof.taveirne@gmail.com>
2017-10-21 22:30:27 +0200

    Use Button style to show on vs off

--- src/main/java/com/biprom/DentalArno/View/KeukenView.java
+++ src/main/java/com/biprom/DentalArno/View/KeukenView.java
@@ -5,6 +5,7 @@ import com.biprom.DentalArno.Design.KeukenVerlichting;
 import com.biprom.DentalArno.extension.DownUpExtension;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringComponent
@@ -19,17 +20,18 @@ public class KeukenView extends KeukenVerlichting {
 		// Tried my best, doesn't work.
 		new DownUpExtension().extend(buttonKeuken1);
 
+		// This works! :)
 		buttonKeuken2.addClickListener(new Button.ClickListener() {
 			@Override
 			public void buttonClick(Button.ClickEvent clickEvent) {
 				if (data.isBoolVKeuken2()) {
 					// button staat aan, afzetten die handelen
 					data.setBoolVKeuken2(false);
-					buttonKeuken2.setCaption("keuken 2");
+					buttonKeuken2.setStyleName(ValoTheme.BUTTON_PRIMARY);
 				} else {
 					// button staat af , aanzetten die handelen
 					data.setBoolVKeuken2(true);
-					buttonKeuken2.setCaption("keuken 2 - ON");
+					buttonKeuken2.setStyleName(ValoTheme.BUTTON_DANGER);
 				}
 			}
 		});
